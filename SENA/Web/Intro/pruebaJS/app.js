2b1f09ccc139587720af4326d3b1515302397f89
Jonathan <jsalgado591@gmail.com>
2026-09-16 19:34:37 -0500

    Agragados talleres html1 y html2

--- SENA/Web/Intro/pruebaJS/app.js
+++ SENA/Web/Intro/pruebaJS/app.js
@@ -1,7 +1,22 @@
-//1. Guardamos y buscamos en HTML por su id exacto
-const titulo = document.getElementById("titulo-principal");
-const mensaje = document.getElementById("mensaje-bienvenida");
+// //1. Guardamos y buscamos en HTML por su id exacto
+// const titulo = document.getElementById("titulo-principal");
+// const mensaje = document.getElementById("mensaje-bienvenida");
 
-//2. Modificamos mensaje interno de cada etiqueta
-titulo.textContent ="Desde JavaScript tomando el control! 🚀"
-mensaje.textContent = "El tiempo fue cambiado en tiempo real"
+// //2. Modificamos mensaje interno de cada etiqueta
+// titulo.textContent ="Desde JavaScript tomando el control! 🚀"
+// mensaje.textContent = "El tiempo fue cambiado en tiempo real"
+
+console.log("Hola desde app.js")
+
+const titulo =document.getElementById("titulo-principal")
+const mensaje = document.getElementById("input-texto")
+const boton = document.querySelector("#boton-cambiar")
+
+//Lectura de acción del usuario
+boton.addEventListener("click", function(){
+    // console.log("El sensor funciona. Hiciste click")
+    const nuevo_titulo = mensaje.value
+    titulo.textContent = nuevo_titulo
+});
+
+
